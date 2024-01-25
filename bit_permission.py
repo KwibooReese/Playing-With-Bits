11f6b87263bc13694215c18edec8567fb1205a7b
KwibooReese <reesehayman@gmail.com>
2024-01-25 17:03:22 +0000

    First Commit

---
+++ bit_permission.py
@@ -0,0 +1,19 @@
+user_permissions = 8
+
+all_permissions = {
+    "speak":1,
+    "woof":2,
+    "meow":4,
+    "roar":8
+}
+
+def speak(string):
+        if not user_has_permissions(all_permissions["speak"]) > 0:
+            print("You do not have permission to speak")
+        else:
+            print(string)
+
+def user_has_permissions(permission_to_look_for):
+     return (user_permissions | permission_to_look_for) == 1
+
+speak("hello")
